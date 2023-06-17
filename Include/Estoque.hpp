a799435bf9867c651ac4f02189f7e4db1fb816b2
FCJar <Fcjardim764@gmail.com>
2023-06-17 20:42:50 -0300

    0.1

---
+++ Include/Estoque.hpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include "Produto.hpp"
+
+class Estoque
+{
+private:
+    std::vector<Produto*> _produtos;
+    int id;
+public:
+    //id deve ser iniciado junto com com construtor
+    Estoque();
+    void retiraProduto();
+    void adcionarProduto();
+    void retiraProdutoPorTipo();
+    void adcionarProdutoPorTipo();
+};
--- Include/Produto.hpp
+++ Include/Produto.hpp
@@ -1,7 +1,17 @@
+#include <string>
+
 class Produto
 {
-private:
-    
-public:
-
+    private:
+        int _Id;   
+        std::string _Nome, _Tipo, _DataValidade;
+        double _Preco, _LucroProduto;
+    public:
+        Produto (int id,std::string nome,std::string tipo,double preco,double lucroProduto);
+        int getId();
+        std::string getNome();
+        std::string getTipo();
+        std::string getDataValidade();
+        double getPreco();
+        double getLucroProduto();
 };

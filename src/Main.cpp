e1c0c9b701be7fd50a5c76b19dbd23507d44b553
FCJar <Fcjardim764@gmail.com>
2023-06-19 17:01:59 -0300

    1.2.1.8

---
+++ src/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Atendente.hpp"
+#include "Caixa.hpp"
+#include "Comanda.hpp"
+#include "Estoque.hpp"
+#include "Funcionario.hpp"
+#include "Gerente.hpp"
+#include "Produto.hpp"
+
+int main(){
+
+
+    return 0;
+};
+
+
+
